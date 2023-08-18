c52ad769f616b730ae6661d5a15fa4754c7366ed
Eominik <dominik.kagw@gmail.com>
2023-08-18 17:19:34 +0200

    Part 21 - SearchViewModel

---
+++ tracker/tracker_presentation/src/main/java/com/example/tracker_presentation/search/SearchState.kt
@@ -0,0 +1,9 @@
+package com.example.tracker_presentation.search
+
+data class SearchState(
+    val query: String = "",
+    val isHintVisible: Boolean = false,
+    val isSearching: Boolean = false,
+    val trackableFood: List<TrackableFoodUiState> = emptyList()
+) {
+}
